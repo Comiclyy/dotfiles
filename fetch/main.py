4517af32aa5d712b938c879c8985d8cc2fb26bfd
Max <93946611+Comicly69@users.noreply.github.com>
2023-05-30 13:25:02 +0000

    mod fetchpy

--- fetch/main.py
+++ fetch/main.py
@@ -1,5 +1,14 @@
-print("        ")
-print(" {\\_/} ")
-print(" (●ᴗ●)  ")
-print(" ( >🥕  ")
-print("        ")
+import colorama
+
+# Initialize colorama
+colorama.init()
+
+# Print the formatted output
+print("           " + colorama.Fore.MAGENTA + "max" + colorama.Style.BRIGHT + "@" + colorama.Style.RESET_ALL + colorama.Fore.MAGENTA + "kinetic" + colorama.Style.RESET_ALL)
+print(colorama.Style.BRIGHT + "           " + colorama.Style.RESET_ALL + "cpu    " + colorama.Fore.LIGHTRED_EX + "{:.2f}%".format(cpuPercentage))
+print(colorama.Style.BRIGHT + "   {\\_/}" + colorama.Style.RESET_ALL + "   mem  " + colorama.Fore.LIGHTRED_EX + "  {}/{} MB".format(usedMemory, totalMemory))
+print(colorama.Style.BRIGHT + "   (●ᴗ●)" + colorama.Style.RESET_ALL + "   dsk   " + colorama.Fore.LIGHTRED_EX + " ({}%)".format(usedDisk))
+print(colorama.Style.BRIGHT + "   ( >🥕)" + colorama.Style.RESET_ALL + "  ip     " + colorama.Fore.LIGHTRED_EX + "{} ".format(ipAddress) + colorama.Fore.LIGHTCYAN_EX + "({})".format(operatingSystem))
+print(colorama.Style.BRIGHT + "           " + colorama.Style.RESET_ALL + "distro" + colorama.Fore.LIGHTRED_EX + " Fedora")
+print(colorama.Style.RESET_ALL + colorama.Style.BRIGHT + "           shell" + colorama.Fore.LIGHTRED_EX + "  bash\n ")
+print(colorama.Style.RESET_ALL)  # Reset color to default
